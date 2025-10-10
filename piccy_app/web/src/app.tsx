41b1eedf199fbcc19691cde409425b269ea5bbfd
wuliya <3369906077@qq.com>
2025-10-10 21:10:46 +0800

    chore(release): 更新 GitHub Release 配置 - 修改 release 文件上传路径为根目录所有文件 - 添加 build-npm-lib 步骤依赖条件判断 - 引入独立的 npm 库构建工作流配置文件

--- piccy_app/web/src/app.tsx
+++ piccy_app/web/src/app.tsx
@@ -59,6 +59,8 @@ export default function App() {
 					}} />
 
 
+
+
 					<Button className={"!bg-gradient-to-r from-pink-300 to-blue-300 mt-10"}
 					        mx="auto"
 					        display="block"
