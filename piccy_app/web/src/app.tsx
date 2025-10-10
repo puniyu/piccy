923c523157ebd88a45aa16762554b0ac11f4fb90
wuliya <3369906077@qq.com>
2025-10-10 20:55:30 +0800

    chore(ci): 更新构建产物名称格式 - 统一为所有平台的构建产物名称添加 app 前缀 - 确保不同平台间的构建产物命名一致性 -便于识别和管理各平台的应用程序包

--- piccy_app/web/src/app.tsx
+++ piccy_app/web/src/app.tsx
@@ -59,7 +59,6 @@ export default function App() {
 					}} />
 
 
-
 					<Button className={"!bg-gradient-to-r from-pink-300 to-blue-300 mt-10"}
 					        mx="auto"
 					        display="block"
