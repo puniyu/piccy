d4694b0af3bfa6fbe4a2b5e5a2e84c78162cf7be
wuliya <3369906077@qq.com>
2026-07-01 18:16:04 +0800

    ci: 移除piccy_node

--- piccy_app/web/src/components/image/index.ts
+++
@@ -1,6 +0,0 @@
-export { ImageInfoListModal } from "./info";
-export { ImageCropModal } from "./crop";
-export { ImageRotateModal } from "./rotate";
-export { ImageResizeModal } from "./resize";
-export { ImageFlipModal } from "./flip";
-export { ImageFilterModal } from "./filter";
